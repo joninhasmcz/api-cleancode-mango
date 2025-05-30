547a8c9910d2091b4c52658c43cc792d34d0b1e8
jonas.santos <jonas-asa-cor@hotmail.com>
2025-05-30 10:26:24 -0300

    feat(auth-usecase): Adicionar validação de senha no método auth e testes relacionados.

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -20,20 +20,25 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class AuthUseCase {
-  async auth(email) {
+  async auth(email, password) {
       if(!email) {
-        return MissingParamError('email')
+        throw new MissingParamError('email')
       }
-
+      if(!password) {
+        throw new MissingParamError('password')
+      }
   }
 }
 
-
-
 describe('Auth UseCase', () => {
-  test('Should return null if no email is provided', async () => {
+  test('Should throw null if no email is provided', async () => {
       const sut = new AuthUseCase()
       const promise =  sut.auth()
-      expect(promise).rejects.toThrow()
+      expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+  test('Should throw null if no password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise =  sut.auth('any_mail@mail.com')
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })

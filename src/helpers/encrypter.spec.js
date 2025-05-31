4ec0461b932446fbe1ebbb92775dfd99f7a20697
Jonas <junas@ic.ufal.br>
2025-05-31 19:47:31 -0300

    test(encrypter): Adicionar testes para garantir erro quando parâmetros estão ausentes

--- src/helpers/encrypter.spec.js
+++ src/helpers/encrypter.spec.js
@@ -1,5 +1,6 @@
 const Encrypter = require('./encrypter')
 const bcrypt = require('bcrypt')
+const {MissingParamError} = require("../utils/errors");
 
 
 
@@ -24,5 +25,11 @@ describe('Encrypter', () => {
         await sut.compare('any_value', 'hashed_value')
         expect(bcrypt.value).toBe('any_value')
         expect(bcrypt.hash).toBe('hashed_value')
+    })
+    test('Should throw if no value is provided', async () => {
+        const sut = makeSut()
+        expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+        expect(sut.compare('any_value')).rejects.toThrow(new MissingParamError('hash'))
+
     })
 })

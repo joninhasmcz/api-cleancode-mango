28b8f16a057965667c0ef66756e756eac5d290c0
Jonas <junas@ic.ufal.br>
2025-05-30 22:05:37 -0300

    test(auth-usecase): Adicionar teste para garantir erro quando dependências lançam exceções

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -14,6 +14,33 @@ const makeEncrypter = () => {
   return encrypterSpy
 }
 
+const makeEncrypterWithError = () => {
+  class EncrypterSpy {
+    async compare () {
+      throw new Error()
+    }
+  }
+  return new EncrypterSpy()
+}
+
+const makeLoadUserByEmailRepositoryWithError = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load () {
+      throw new Error()
+    }
+  }
+  return new LoadUserByEmailRepositorySpy()
+}
+
+const makeTokenGeneratorWithError = () => {
+  class TokenGeneratorSpy {
+    async generate () {
+      throw new Error()
+    }
+  }
+  return new TokenGeneratorSpy()
+}
+
 const makeTokenGenerator = () => {
   class TokenGeneratorSpy {
     async generate (userId) {
@@ -148,6 +175,29 @@ describe('Auth UseCase', () => {
               tokenGenerator: invalid
             }
         ),
+    )
+    for (const sut of suts) {
+      const promise = sut.auth('any_mail@mail.com', 'any_password')
+      expect(promise).rejects.toThrow()
+    }
+  })
+  test('Should throw if any depedency throws', async () => {
+    const loadUserByEmailRepository = makeLoadUserByEmailRepository()
+    const encrypter = makeEncrypter()
+
+    const suts  = [].concat(
+        new AuthUseCase({
+          loadUserByEmailRepository: makeLoadUserByEmailRepositoryWithError()
+        }),
+        new AuthUseCase({
+          loadUserByEmailRepository,
+          encrypter: makeEncrypterWithError()
+        }),
+        new AuthUseCase({
+          loadUserByEmailRepository,
+          encrypter,
+          tokenGenerator: makeTokenGeneratorWithError()
+        }),
     )
     for (const sut of suts) {
       const promise = sut.auth('any_mail@mail.com', 'any_password')
